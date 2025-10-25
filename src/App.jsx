63940d6d05962370ca4c93b0824e908202d381ca
Stivii <aim.medina@duocuc.cl>
2025-10-25 16:30:57 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function Layout() {
     }
   }, [location.pathname]);
 
-  const hideNavbarRoutes = ["/auth"];
+  const hideNavbarRoutes = ["/auth", "/homeadmin"];
   const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
   const shouldShowBotonWsp = !hideNavbarRoutes.includes(location.pathname);
 
